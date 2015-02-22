d6f18c53a8d074696eb8e15716533b47467dbcc9
Jacob Lowe <jacoblowe2.0@gmail.com>
2015-02-22 12:37:48 -0800

    removing commas

--- src/encode.js
+++ src/encode.js
@@ -1,4 +1,4 @@
-'use strict';
+'use strict'
 
 var sortValues = {
         x1: 0,
@@ -12,14 +12,12 @@ var sortValues = {
         sweep: 8,
         x: 9,
         y: 10
-    };
+    }
 
 /*
-    vsvg-paths
-    ---------------------------------------------------
-    this is a small module to encode decode svg pathing
-
-    encode - encode object collection to path string
+    vsvg-paths.encode - encode object collection to path string
+    ------------------------------------------------------------------
+    
     params
         path { Array } - an array of points some exmaple points ar as follows
             [ 
@@ -32,6 +30,8 @@ var sortValues = {
             ]
             with encode to: M5 5 L100 100 C100 100 250 100 250 200 S400 300 400 200 H500 V100
         isRelative { Boolean } - flag to make paths relative
+    returns
+        pathData { String } - the encoded string 
 
 */
 
@@ -39,104 +39,103 @@ module.exports.encode =
 function encode( path, isRelative ) {
     
     if ( !Array.isArray( path ) ) {
-        return;
+        return
     }
     var instructions = path.map( getInstruction ),
-        values;
+        values
 
     if ( isRelative ) {
-        instructions = instructions.map( mapLowerCase );
+        instructions = instructions.map( mapLowerCase )
     }
     values = path.map( keyValueArray ) // [ [ [ y, 0 ], [ x, 0 ] ] ]
             .map( sortPoints ) // [ [ x, 0 ], [ y, 0 ] ] according to sort values
-            .map( stringifyPoints( instructions ) );  // [ 'M0 0' ];
+            .map( stringifyPoints( instructions ) )  // [ 'M0 0' ]
     
-    return values.join( ' ' );
-};
+    return values.join( ' ' )
+}
 
 var keyValueArray =
 module.exports._keyValueArray =
 function keyValueArray( points ) {
-    var ret = [];
+    var ret = []
     for( var key in points ) {
-        ret.push( [ key, points[ key ] ] );
+        ret.push( [ key, points[ key ] ] )
     }
-    return ret;
-};
+    return ret
+}
 
 var mapLowerCase =
 module.exports._mapLowerCase = 
 function mapLowerCase ( str ) {
-    return str.toLowerCase();
-};
+    return str.toLowerCase()
+}
 
 var getValue =
 module.exports._getValue =
 function getValue(  instr ) {
     return function ( point, index ) {
-        return ( !index ? instr : '' ) + point[ 1 ];
+        return ( !index ? instr : '' ) + point[ 1 ]
     }
-};
+}
 
 var sortValue =
 module.exports._sortValue =
 function sortValue ( prev, cur ) {
-    return sortValues[ prev[ 0 ] ] - sortValues[ cur[ 0 ] ];
-};
+    return sortValues[ prev[ 0 ] ] - sortValues[ cur[ 0 ] ]
+}
 
 var sortPoints =
 module.exports._sortPoints =
 function sortPoints ( points ) {
-    return points.sort( sortValue );
-};
+    return points.sort( sortValue )
+}
 
 var stringifyPoints =
 module.exports._stringifyPoints =
 function stringifyPoints( instructions ) {
     return function( point, index ) {
         if ( !point.length ) {
-            return instructions[ index ]; // should be a close
+            return instructions[ index ] // should be a close
         }
-        return point.map( getValue( instructions[ index ] ) ).join( ' ' );
+        return point.map( getValue( instructions[ index ] ) ).join( ' ' )
     }
-};
+}
 
 var isValid =
 module.exports._isValid =
 function isValid ( value ) {
     if ( isNaN( value ) ) {
-        return false;
+        return false
     }
-    return value || typeof value === 'number';
-};
+    return value || typeof value === 'number'
+}
 
 var getInstruction =
 module.exports._getInstruction =
 function getInstruction ( points, index ) {
     if ( isValid( points.x2 )  && isValid( points.y2 ) && isValid( points.x1 ) && isValid( points.y1 ) ) {
-        return 'C';
+        return 'C'
     }
     if ( isValid( points.rx ) && isValid( points.ry ) ) {
-        return 'A';
+        return 'A'
     }
     if ( isValid( points.x1 ) && isValid( points.y1 ) ) {
-        return 'Q';
+        return 'Q'
     }
     if ( isValid( points.x2 ) && isValid( points.y2 ) ) {
-        return 'S';
+        return 'S'
     }
     if ( isValid( points.x ) && isValid( points.y ) ) {
         if ( !index ) {
-            return 'M';
+            return 'M'
         }
-        return 'L';
+        return 'L'
     }
     if ( isValid( points.x ) ) {
-        return 'H';
+        return 'H'
     }
     if ( isValid( points.y ) ) {
-        return 'V';
+        return 'V'
     }
-    return 'Z';
-};
-
+    return 'Z'
+}
